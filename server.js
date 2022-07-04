e9aebd594388ba105bb23cafba99fb704fc9d23b
Brandon Artigue <bartigue87@yahoo.com>
2022-07-03 23:44:29 -0600

    trying to fix deploy erros

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const usersRoutes = require("./backend/routes/users-routes");
 const trackerRoutes = require("./backend/routes/tracker-routes");
 const historyRoutes = require("./backend/routes/history-routes");
 const HttpError = require("./backend/models/http-error");
+require("dotenv").config();
 
 const app = express();
 
@@ -14,15 +15,15 @@ app.use(bodyParser.json());
 
 app.use(express.static(path.join("public")));
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   );
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+//   next();
+// });
 
 app.use("/api/trackers", trackerRoutes);
 app.use("/api/users", usersRoutes);
